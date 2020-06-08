f50a974d128a99e2e78055086658861b387b2cb7
Jonathan Linnestad <jonathan.s.linnestad@gmail.com>
2020-06-08 14:41:14 +0200

    Changes to intermediate points, checks every meter

--- src/altitude.js
+++ src/altitude.js
@@ -1,12 +1,19 @@
 import axios from 'axios';
 import { getUTMFormat } from './utm';
 
-const lnglatToAltitude = async (latitude, longitude) => {
+export const lngLatToAltitude = async (latitude, longitude) => {
   const pos = getUTMFormat({ latitude, longitude, zoneNumber: 33 });
   const res = await axios.get(`https://hoydedata.no/arcgis/rest/services/dtm1_33/ImageServer/identify?f=json&geometryType=esriGeometryPoint&geometry={%22x%22:${pos.Easting},%22y%22:${pos.Northing},%22spatialReference%22:{%22wkid%22:25833,%22latestWkid%22:25833}}`)
     .then((response) => response.data.value).catch((err) => console.log(err));
   return res;
 };
 
+export const lnglatToAltitudeBetweenPoints = async (coord1, coord2) => {
+  const pos1 = getUTMFormat({ longitude: coord1.longitude, latitude: coord1.latitude, zoneNumber: 33 });
+  const pos2 = getUTMFormat({ longitude: coord2.longitude, latitude: coord2.latitude, zoneNumber: 33 });
 
-export default lnglatToAltitude;
+  const both = await Promise.all([
+    axios.get(`https://hoydedata.no/arcgis/rest/services/Terrengprofil/GPServer/Terrengprofil/execute?f=json&env%3AoutSR=25833&InputLineFeatures=%7B%22fields%22%3A%5B%7B%22name%22%3A%22OID%22%2C%22type%22%3A%22esriFieldTypeObjectID%22%2C%22alias%22%3A%22OID%22%7D%5D%2C%22geometryType%22%3A%22esriGeometryPolyline%22%2C%22features%22%3A%5B%7B%22geometry%22%3A%7B%22paths%22%3A%5B%5B%5B${pos1.Easting}%2C${pos1.Northing}%5D%2C%5B${pos2.Easting}%2C${pos2.Northing}%5D%5D%5D%2C%22spatialReference%22%3A%7B%22wkid%22%3A25833%2C%22latestWkid%22%3A25833%7D%7D%2C%22attributes%22%3A%7B%22OID%22%3A1%7D%7D%5D%2C%22sr%22%3A%7B%22wkid%22%3A25833%2C%22latestWkid%22%3A25833%7D%7D&ProfileIDField=OID&DEMResolution=FINEST&MaximumSampleDistance=78204.56092215818&MaximumSampleDistanceUnits=Meters&returnZ=true&returnM=true&ElevationType=dtm`).then((response) => response.data).catch((err) => console.log(err)),
+    axios.get(`https://hoydedata.no/arcgis/rest/services/Terrengprofil/GPServer/Terrengprofil/execute?f=json&env%3AoutSR=25833&InputLineFeatures=%7B%22fields%22%3A%5B%7B%22name%22%3A%22OID%22%2C%22type%22%3A%22esriFieldTypeObjectID%22%2C%22alias%22%3A%22OID%22%7D%5D%2C%22geometryType%22%3A%22esriGeometryPolyline%22%2C%22features%22%3A%5B%7B%22geometry%22%3A%7B%22paths%22%3A%5B%5B%5B${pos1.Easting}%2C${pos1.Northing}%5D%2C%5B${pos2.Easting}%2C${pos2.Northing}%5D%5D%5D%2C%22spatialReference%22%3A%7B%22wkid%22%3A25833%2C%22latestWkid%22%3A25833%7D%7D%2C%22attributes%22%3A%7B%22OID%22%3A1%7D%7D%5D%2C%22sr%22%3A%7B%22wkid%22%3A25833%2C%22latestWkid%22%3A25833%7D%7D&ProfileIDField=OID&DEMResolution=FINEST&MaximumSampleDistance=78204.56092215818&MaximumSampleDistanceUnits=Meters&returnZ=true&returnM=true&ElevationType=dom`).then((response) => response.data).catch((err) => console.log(err))]);
+  return { dtm: both[0].results[0].value.features[0].geometry.paths, dom: both[1].results[0].value.features[0].geometry.paths };
+};
--- src/path.js
+++ src/path.js
@@ -1,7 +1,8 @@
 import {
   getPreciseDistance, computeDestinationPoint, getGreatCircleBearing,
 } from 'geolib';
-import lngLatToAltitude from './altitude';
+import { lngLatToAltitude, lnglatToAltitudeBetweenPoints } from './altitude';
+import { getLatLngFormat } from './utm';
 
 
 const calculateIntermediatePoints = ({
@@ -30,11 +31,11 @@ const altitudesFromPoints = async (points) => {
   return pointsWithAltitude;
 };
 
-const removeUnnecessaryPoints = ({ coordinates, maxiumumHeightDifference }) => {
+const removeUnnecessaryPoints = ({ coordinates, maximumHeightDifference }) => {
   const necessaryCoordinates = [coordinates[0]];
   for (let index = 1; index < coordinates.length - 1; index += 1) {
     const element = coordinates[index];
-    if (Math.abs(element.altitude - necessaryCoordinates[necessaryCoordinates.length - 1].altitude) > maxiumumHeightDifference) {
+    if (Math.abs(element.altitude - necessaryCoordinates[necessaryCoordinates.length - 1].altitude) > maximumHeightDifference) {
       necessaryCoordinates.push(element);
     }
   }
@@ -42,36 +43,109 @@ const removeUnnecessaryPoints = ({ coordinates, maxiumumHeightDifference }) => {
   return necessaryCoordinates;
 };
 
-const coordinatesBetweenPoints = async ({ coordinates, precision, maxiumumHeightDifference }) => {
+const coordinatesBetweenPoints = async ({ coordinates, precision, maximumHeightDifference }) => {
   const [coordinate1, coordinate2] = coordinates;
   const intermediatePoints = intermediatePointsFromCoordinates({ coordinates, precision });
   const allPoints = [...[coordinate1], ...intermediatePoints, ...[coordinate2]];
   const allPointsWithAltitude = await altitudesFromPoints(allPoints);
   const necessaryCoordinates = removeUnnecessaryPoints({
     coordinates: allPointsWithAltitude,
-    maxiumumHeightDifference,
+    maximumHeightDifference,
   });
   return necessaryCoordinates;
 };
 
+
+const preferedAltitude = (coordinate, height, minimumClearence) => {
+  if (height + coordinate.dtm > coordinate.dom + minimumClearence) return coordinate.dtm + height;
+  return coordinate.dom + minimumClearence;
+};
+
+const formatDomAndDtm = (coordinates) => {
+  const { dtm, dom } = coordinates;
+  const formatted = dtm[0].map((dtmElement, index) => {
+    const asLngLng = getLatLngFormat({
+      Easting: dtmElement[0],
+      Northing: dtmElement[1],
+      zoneNumber: 33,
+      zoneLetter: 'V',
+    });
+    const format = {
+      longitude: asLngLng.lng, latitude: asLngLng.lat, dtm: dtmElement[2], dom: dom[0][index][2], meters: dom[0][index][3], Northing: dtmElement[1], Easting: dtmElement[0],
+    };
+    console.log(format.dom);
+    return format;
+  });
+  return formatted;
+};
+
+const removeUnnecessaryPoints2 = ({
+  coordinates, maximumHeightDifference, height, minimumClearence,
+}) => {
+  const necessaryCoordinatesLeft = [];
+  necessaryCoordinatesLeft[0] = { ...coordinates[0], altitude: preferedAltitude(coordinates[0], height, minimumClearence) };
+  for (let index = 1; index < coordinates.length - 1; index += 1) {
+    const element = coordinates[index];
+    const prefAlt = preferedAltitude(element, height, minimumClearence);
+    console.log();
+    if (Math.abs(prefAlt - necessaryCoordinatesLeft[necessaryCoordinatesLeft.length - 1].altitude) > maximumHeightDifference || prefAlt - necessaryCoordinatesLeft[necessaryCoordinatesLeft.length - 1].altitude < -maximumHeightDifference * 2) {
+      necessaryCoordinatesLeft.push({ ...element, altitude: prefAlt });
+    }
+  }
+  necessaryCoordinatesLeft.push({ ...coordinates[coordinates.length - 1], altitude: preferedAltitude(coordinates[coordinates.length - 1], height, minimumClearence) });
+
+  return necessaryCoordinatesLeft;
+};
+
+const coordinatesAndAltitudeBetweenPoints = async ({
+  coordinates, maximumHeightDifference, minimumClearence, height,
+}) => {
+  const [coordinate1, coordinate2] = coordinates;
+  const allPoints = await lnglatToAltitudeBetweenPoints(coordinate1, coordinate2);
+  const formatted = formatDomAndDtm(allPoints);
+  const necessaryCoordinates = removeUnnecessaryPoints2({
+    coordinates: formatted,
+    maximumHeightDifference,
+    minimumClearence,
+    height,
+  });
+  return necessaryCoordinates;
+};
+
+const test = async () => {
+  const point1 = { longitude: 10.988444685229664, latitude: 62.406867964261586 };
+  const point2 = { longitude: 10.989672004948714, latitude: 62.405606891565995 };
+  const points = await coordinatesAndAltitudeBetweenPoints({
+    coordinates: [point1, point2], maximumHeightDifference: 5, height: 20, minimumClearence: 5,
+  });
+  console.log(points);
+};
+
+
+test();
 const allCoordinatesFromPath = async ({
-  path, precision = 20, maxiumumHeightDifference = 10, height,
+  path, maximumHeightDifference = 5, height,
 }) => {
   let pathWithAltitudes = [];
   const promises = [];
-  console.log(path);
-  for (let index = 0; index < path.length - 1; index += 1) {
-    promises[index] = coordinatesBetweenPoints({
-      coordinates: [path[index],
-        path[index + 1]],
-      precision,
-      maxiumumHeightDifference,
+  const withoutClosePoints = [path[0]];
+  for (let index = 1; index < path.length; index += 1) {
+    if (getPreciseDistance(withoutClosePoints[withoutClosePoints.length - 1], path[index]) > 1) {
+      withoutClosePoints.push(path[index]);
+    }
+  }
+  for (let index = 0; index < withoutClosePoints.length - 1; index += 1) {
+    promises[index] = coordinatesAndAltitudeBetweenPoints({
+      coordinates: [withoutClosePoints[index],
+        withoutClosePoints[index + 1]],
+      maximumHeightDifference,
+      height,
+      minimumClearence: 5,
+      minimumDistance: 10,
     });
   }
-  console.log(promises);
   const coordinates = await Promise.all(promises);
-  console.log(coordinates);
-  for (let index = 0; index < path.length - 1; index += 1) {
+  for (let index = 0; index < withoutClosePoints.length - 1; index += 1) {
     if (index === 0) {
       pathWithAltitudes = pathWithAltitudes.concat(coordinates[index]);
     } else {
